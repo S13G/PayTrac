bb3f82ce1f15d86675b85d3a52cb469ec8533e5d
s13g <ayflix0@gmail.com>
2023-11-26 06:05:49 +0100

    updated vercel config

--- paytrac/settings.py
+++ paytrac/settings.py
@@ -205,7 +205,7 @@ STATIC_URL = "static/"
 
 STATICFILES_DIRS = [BASE_DIR / "static"]
 
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles", "static")
 
 STORAGES = {
     "default": {
