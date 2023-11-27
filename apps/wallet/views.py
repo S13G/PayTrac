97d38bbb9ec2fd04099d132e8ff46ab8d0853e51
s13g <ayflix0@gmail.com>
2023-11-27 10:07:36 +0100

    transaction tracking done

--- apps/wallet/views.py
+++ apps/wallet/views.py
@@ -1,7 +1,19 @@
+import requests
+from django.conf import settings
+from django.utils import timezone
 from drf_spectacular.utils import extend_schema, OpenApiResponse
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
+
+from apps.common.errors import ErrorCode
+from apps.common.exceptions import RequestError
+from apps.common.responses import CustomResponse
+
+headers = {
+    'Content-Type': 'application/json',
+    'Authorization': f"Bearer {settings.FW_SECRET_KEY}"
+}
 
 
 # Create your views here.
@@ -20,4 +32,45 @@ class TrackingTransactionsView(APIView):
         }
     )
     def get(self, request):
-        return status.HTTP_200_OK
+        user = request.user
+        order_ref = user.wallet.order_ref
+        url = f"https://api.flutterwave.com/v3/transactions"
+        wallet_created = user.wallet.created.strftime("%Y-%m-%d")
+        business_owner_email = user.email
+
+        data = {
+            "from": wallet_created,
+            "to": timezone.now().strftime("%Y-%m-%d"),
+        }
+
+        try:
+            response = requests.get(url, headers=headers, params=data).json()
+            response_page_data = response.get('meta').get('page_info')
+            response_data = response.get('data')
+        except Exception as e:
+            raise RequestError(err_code=ErrorCode.FAILED, err_msg=str(e), status_code=status.HTTP_400_BAD_REQUEST)
+
+        data = {
+            "total_transactions": response_page_data.get('total'),
+            "page_number": response_page_data.get('current_page'),
+            "total_pages": response_page_data.get('total_pages'),
+            "transactions": [
+                {
+                    "id": transaction['id'],
+                    "invoice_number": transaction['meta'].get('invoice number', 'N/A'),
+                    "transaction_reference": transaction['tx_ref'],
+                    "status": transaction['status'],
+                    "charged_amount": transaction['charged_amount'],
+                    "app_fee": transaction['app_fee'],
+                    "amount_settled": transaction['amount_settled'],
+                    "currency": transaction['currency'],
+                    "created_at": transaction['created_at'],
+                    "payment_type": transaction['payment_type'],
+                    "customer_email": transaction['customer']['email'],
+                    "customer_name": transaction['customer']['name'],
+                    "customer_bank_name": transaction['meta'].get('bankname', 'N/A'),
+                }
+                for transaction in response_data
+            ]
+        }
+        return CustomResponse.success(message="Retrieved all transactions successfully", data=data)
